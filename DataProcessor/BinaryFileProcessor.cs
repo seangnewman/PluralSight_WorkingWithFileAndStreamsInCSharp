dfe75be63e51822100c2c3b2608e3b9681314544
seangnewman <seangnewman@msn.com>
2021-04-02 05:28:49 -0700

    Review of streams and incrementally writing/appending data

--- DataProcessor/BinaryFileProcessor.cs
+++ DataProcessor/BinaryFileProcessor.cs
@@ -17,15 +17,34 @@ namespace DataProcessor
 
         public void Process()
         {
-            byte[] data = File.ReadAllBytes(InputFilePath);
-            byte largest = data.Max();
+            //byte[] data = File.ReadAllBytes(InputFilePath);
+            //byte largest = data.Max();
 
-            byte[] newData = new byte[data.Length + 1];
-            Array.Copy(data, newData, data.Length);
-            newData[newData.Length - 1] = largest;
+            //byte[] newData = new byte[data.Length + 1];
+            //Array.Copy(data, newData, data.Length);
+            //newData[newData.Length - 1] = largest;
 
-            File.WriteAllBytes(OutputFilePath, newData);
+            //File.WriteAllBytes(OutputFilePath, newData);
+            using (FileStream inputFileStream = File.Open(InputFilePath, FileMode.Open, FileAccess.Read))
+            using (BinaryReader binaryStreamReader = new BinaryReader(inputFileStream))
+            using (FileStream outputFileStream = File.Create(OutputFilePath))
+             using (BinaryWriter binaryStreamWriter = new BinaryWriter(outputFileStream))
+            {
+                int largest = 0;
 
+                while (binaryStreamReader.BaseStream.Position < binaryStreamReader.BaseStream.Length)
+                {
+                    int currentByte = binaryStreamReader.ReadByte();
+
+                    binaryStreamWriter.Write(currentByte);
+
+                    if (currentByte > largest)
+                    {
+                        largest = currentByte;
+                    }
+                }
+                binaryStreamWriter.Write(largest);
+            }
 
         }
     }
--- DataProcessor/TextFileProcessor.cs
+++ DataProcessor/TextFileProcessor.cs
@@ -1,6 +1,8 @@
 ﻿using System.IO;
 using System.Text;
+using System;
 namespace DataProcessor
+  
 {
     class TextFileProcessor
     {
@@ -21,9 +23,85 @@ namespace DataProcessor
             //File.WriteAllText(OutputFilePath, processedTest);
 
             // Using read all lines
-            string[] lines = File.ReadAllLines(InputFilePath);
-            lines[1] = lines[1].ToUpperInvariant();
-            File.WriteAllLines(OutputFilePath, lines);
+            //string[] lines = File.ReadAllLines(InputFilePath);
+            //lines[1] = lines[1].ToUpperInvariant();
+            //File.WriteAllLines(OutputFilePath, lines);
+
+            // using (var inputFileStream = new FileStream(InputFilePath, FileMode.Open))
+            //    using(var inputStreamReader = new StreamReader(inputFileStream))
+            //    using(var outputFileStream = new FileStream(OutputFilePath, FileMode.Create))
+            //    using (var outputStreamWriter = new StreamWriter(outputFileStream))
+            //{
+            //    while (!inputStreamReader.EndOfStream)
+            //    {
+            //        string line = inputStreamReader.ReadLine();  // Only one line in memory as processing
+            //        string processedLine = line.ToUpperInvariant();
+            //        outputStreamWriter.WriteLine(processedLine);
+
+            //    }
+            //}
+
+            //using (var inputStreamReader = new StreamReader(InputFilePath)) 
+            //  using (var outputStreamWriter = new StreamWriter(OutputFilePath))
+            //  {
+            //    while (!inputStreamReader.EndOfStream)
+            //    {
+            //        string line = inputStreamReader.ReadLine();  // Only one line in memory as processing
+            //        string processedLine = line.ToUpperInvariant();
+
+            //        bool isLastLine = inputStreamReader.EndOfStream;
+
+            //        if (isLastLine)
+            //        {
+            //            outputStreamWriter.Write(processedLine);
+            //        }
+            //        else
+            //        {
+            //            outputStreamWriter.WriteLine(processedLine);
+            //        }
+
+            //    }
+
+
+                using (var inputStreamReader = new StreamReader(InputFilePath))
+                using (var outputStreamWriter = new StreamWriter(OutputFilePath))
+                {
+                   var currentLineNumber = 1;
+
+                while (!inputStreamReader.EndOfStream)
+                {
+                    string line = inputStreamReader.ReadLine();
+                    if (currentLineNumber == 2)
+                    {
+                        Write(line.ToUpperInvariant());
+
+                    }
+                    else
+                    {
+                        Write(line);
+                    }
+
+
+
+                    currentLineNumber++;
+                }
+                   void Write(string content)
+                   {
+
+                    bool isLastLine = inputStreamReader.EndOfStream;
+                    if (isLastLine)
+                    {
+                        outputStreamWriter.Write(content);
+                    }
+                    else
+                    {
+                        outputStreamWriter.WriteLine(content);
+                    }
+
+                }
+                }
+
+
 
 
 
